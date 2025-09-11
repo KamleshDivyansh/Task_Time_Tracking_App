8823ce1337e02814b5c8d89047580853b4dc26bb
KamleshDivyansh <divyanshkamlesh70@gmail.com>
2025-09-11 13:55:50 +0530

    editing login.jsx file

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { TextField, Button, Typography, Alert, Container, Paper, Box } from '@mui/material';
